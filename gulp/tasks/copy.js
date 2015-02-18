1ecc2f27639a8711638cc2a1829725a72ebf3017
Josef Jezek <josef@jezek.co.cz>
2015-02-18 14:14:07 +0100

    Better copy task

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,29 +2,23 @@
 
 // Copy All Files At The Root Level (app)
 module.exports = function (gulp, plugins, config) { return function () {
-	var merge = require('merge-stream');
-	var app = gulp.src([
-		'app/*',
-		'!app/test',
-		'!app/*.jade'
-	], {
-		dot: true
-	}).pipe(gulp.dest('dist'));
+	return require('merge-stream')(
+		gulp.src([
+				'app/*',
+				'!app/test',
+				'!app/*.jade'
+			], {
+				dot: true
+			}).pipe(gulp.dest('dist')),
 
-	var bower = gulp.src([
-		'bower_components/**/*'
-	]).pipe(gulp.dest('dist/bower_components'));
+		gulp.src('bower_components/**/*')
+			.pipe(gulp.dest('dist/bower_components')),
 
-	var elements = gulp.src([
-			'.tmp/elements/**/*.html',
-			'!.tmp/elements/elements.html'
-		])
-		.pipe(gulp.dest('dist/elements'));
-
-	var vulcanized = gulp.src(['.tmp/elements/elements.html'])
-		.pipe(plugins.rename('elements.vulcanized.html'))
-		.pipe(gulp.dest('dist/elements'));
-
-	return merge(app, bower, elements, vulcanized)
-		.pipe(plugins.size({title: 'copy'}));
+		gulp.src('.tmp/elements/**/*.html')
+			.pipe(gulp.dest('dist/elements')),
+			
+		gulp.src(['.tmp/elements/elements.html'])
+			.pipe(plugins.rename('elements.vulcanized.html'))
+			.pipe(gulp.dest('dist/elements'))
+	).pipe(plugins.size({title: 'copy'}));
 };};
